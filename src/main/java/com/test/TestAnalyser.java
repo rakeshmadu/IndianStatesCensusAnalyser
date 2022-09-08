ef503e3b422258a9cb05f27dd9e524698e0e593e
Rakeshmadu <rakeshmadu24@gmail.com>
2022-09-08 08:50:49 +0530

    uc1

--- src/main/java/com/test/TestAnalyser.java
+++ src/main/java/com/test/TestAnalyser.java
@@ -1,1 +1,14 @@
+package com.test;
 
+import org.junit.Test;
+
+import com.analyser.StateCensusAnalyser;
+
+public class TestAnalyser {
+
+	@Test
+	public void testCensusAnalyser() {
+		StateCensusAnalyser analyser = new StateCensusAnalyser();
+		analyser.loadData();
+	}
+}
